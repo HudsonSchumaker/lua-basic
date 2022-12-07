d18159ee7c0222adfd29dc02b0679940ec9f7897
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2022-12-07 22:40:18 +0100

    Update for-loops.lua

--- for-loops.lua
+++ for-loops.lua
@@ -1,1 +1,12 @@
 
+--  for loop
+for i = 1, 10, 1 do 
+  io.write(i)
+end
+
+names = { "Alberto", "Julia" "Sophia", "Caroline" }
+
+-- loop tables 
+for k, v pairs(names) do 
+  io.write(v, " ")
+end 
