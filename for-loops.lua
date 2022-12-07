0983935aadadadc36c93fbf9467a73e0ab6499b6
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2022-12-07 22:53:30 +0100

    Update for-loops.lua

--- for-loops.lua
+++ for-loops.lua
@@ -1,4 +1,24 @@
 
+-- while loop
+
+i = 1
+while(1 <= 10) do
+  io.write(i)
+  i = i + 1
+  
+  if i == 8 then break end -- break, there is no continiue  
+end 
+
+
+-- repeat 
+
+repeat
+  io.write("Enter your guess:")
+  guess = io.read()
+
+until to number(guess) == 8 
+
+
 --  for loop
 for i = 1, 10, 1 do 
   io.write(i)
