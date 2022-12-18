09476b43eeae5586c8a923e9d77f2ee70b95fa70
Hudson Luiz Sales Schumaker <hudson.schumaker@me.com>
2022-12-18 15:11:49 +0100

    Update conditionals.lua

--- conditionals.lua
+++ conditionals.lua
@@ -1,1 +1,19 @@
 
+-- Realational Operators: > < >= <= == ~=
+
+-- Logical Operators: and or not 
+
+age = 13
+
+-- if
+if age < 15 then
+  io.write("you can not vote")
+end
+
+-- if else
+
+if age < 15 then
+  io.write("you can not vote")
+else
+  io.write("you can vote")
+end
